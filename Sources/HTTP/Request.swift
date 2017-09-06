05b56cb34ea38c3b8dca377cba1840bfbfaa6865
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-09-06 17:37:27 +0000

    Encode Host header by default

--- Sources/HTTP/Request.swift
+++ Sources/HTTP/Request.swift
@@ -59,6 +59,12 @@ extension Request {
         self.method = method
         self.url = url
         self.version = .oneOne
+        if var host = url.host {
+            if let port = url.port {
+                host.append(":\(port)")
+            }
+            self.host = host
+        }
     }
 }
 
