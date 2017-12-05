51181be9981bf13fef899b10441620f641485a85
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-05 06:32:37 +0000

    Add URLFormEncoded helper

---
+++ Sources/HTTP/UrlFormEncoded.swift
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 Tris Foundation and the project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License
+ *
+ * See LICENSE.txt in the project root for license information
+ * See CONTRIBUTORS.txt for the list of the project authors
+ */
+
+import KeyValueCodable
+
+struct URLFormEncoded {
+    static func encode<T: Encodable>(
+        _ object: T
+    ) throws -> [UInt8] {
+        let values = try KeyValueEncoder().encode(object)
+        let query = URL.Query(values: values)
+        var urlEncodedBytes = [UInt8]()
+        query.encode(to: &urlEncodedBytes)
+        return urlEncodedBytes
+    }
+
+    // FIXME: the same interface shadows the generic one
+    static func encode(
+        encodable object: Encodable
+    ) throws -> [UInt8] {
+        let values = try KeyValueEncoder().encode(encodable: object)
+        let query = URL.Query(values: values)
+        var urlEncodedBytes = [UInt8]()
+        query.encode(to: &urlEncodedBytes)
+        return urlEncodedBytes
+    }
+}
