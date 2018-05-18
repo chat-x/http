7073c7c912904b78240850bd3ebf3cb2f5361c10
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-05-18 20:06:29 +0000

    Remove Foundation from URLParamMatcher

--- Sources/HTTP/Server/URLParamMatcher.swift
+++ Sources/HTTP/Server/URLParamMatcher.swift
@@ -9,13 +9,11 @@
  *                                                                            *
  ******************************************************************************/
 
-import Foundation
-
 public struct URLParamMatcher {
     public let params: [(name: String, index: Int)]
 
     public init(_ url: String) {
-        let components = url.components(separatedBy: "/")
+        let components = url.split(separator: "/")
         var params: [(name: String, index: Int)] = []
         let count = components.count
         for (index, item) in components.enumerated() {
